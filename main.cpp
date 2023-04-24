d0a6396d74d68ce122048f2f8b8590e8475e3354
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-24 10:01:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n, a=1,b=1,c ;
+    cin >> n;
+    if (n>=2){
+        cout << a << b;
+        for( int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c;
+        }
+    }
+    else{
+        cout << "errore";
+    }
+    return 0;
 }
