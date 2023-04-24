a0df2f6fcef2edd8c8e7c6f35a54ff5ee63eb7c6
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-24 10:05:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
     int n, a=1,b=1,c ;
     cin >> n;
     if (n>=2){
-        cout << a << b;
+        cout << a << endl;
+        cout << b << endl;
         for( int i=1;i<n;i++){
             c=a+b;
             a=b;
             b=c;
-            cout << c;
+            cout << c << endl;
         }
     }
     else{
